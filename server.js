d9de4aa957bc1037f3a539bcace2a72505bbd4f3
unknown <megabeastof@gmail.com>
2025-02-27 18:14:07 +0800

    Signed-off-by: unknown <megabeastof@gmail.com>

--- server.js
+++ server.js
@@ -30,7 +30,18 @@ const routes = {
 const app = express();
 // const app = express(); // Duplicate declaration removed
 app.use(express.json()); // Parse JSON request bodies
-app.use(cors()); // Enable CORS for all routes
+app.use(cors({
+  origin: 'http://127.0.0.1:5000', // Allow requests from your frontend
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type, Authorization'
+}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5000'); // Allow frontend
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.options('*', cors()); // Handle preflight requests
 
 // Sample POST endpoint
